aee5bb432675735385ab22ddfb7ead2b8c743ed5
mshirazkamran <blogger31625@gmail.com>
2025-05-11 13:16:03 +0500

    added method to get url of filebin

--- main.py
+++ main.py
@@ -1,18 +1,47 @@
 import requests
 
-req: requests = requests.get("https://filebin.net");
-
-# print(req.text);
-binURL: int = str(req.text).find("binURL");
-print(binURL);
-temp: str = str(req.text);
-url: str = temp[1313:1340];
-print(url);
 
 
-# TODO: calculate the binURL dymanically 
+def getFilebinURL() -> str:
+
+    req = requests.get("https://filebin.net");
+
+    binURL_index: int = str(req.text).find("binURL"); #binURL is the variable name of the 
+    # print(binURL_index);                              #resource endpoint
+
+    # TODO: calculate the binURL dymanically 
+    start_index: int = binURL_index;
+    start_index += 11; 
+    end_index: int = start_index + 20;
+
+    # print(start_index, end_index);
+
+    temp: str = str(req.text);
+    url: str = temp[start_index:end_index]; # [1324:1344] 
+    # print(url);
+
+    finalURL = url.strip().rstrip('";');
+    print("final url is: " + finalURL);
+
+    return finalURL;
 
 
-b = temp[1324:1340]
-print(b + "\n");
+class FileDetails:
+    def __init__(self, filepath: str, filetype: str):
+        self.filepath = filepath;
+        self.filetype = filetype;
 
+
+def httpMethodToFilebin(method: int, fileDetails: FileDetails) -> None:
+    
+    allowedMethods = {
+        1: "get",
+        2: "post",
+        3: "delete"
+    }
+    print("hello");
+
+
+# TODO: make details for bin methods too 
+
+url: str = getFilebinURL();
