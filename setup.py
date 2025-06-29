6e11bf7ca9dbd3db8cbad4d57f79ef1840f8b598
mshirazkamran <blogger31625@gmail.com>
2025-06-29 10:39:21 +0500

    update: added setup.py file

---
+++ setup.py
@@ -0,0 +1,19 @@
+from importlib.metadata import entry_points
+from setuptools import find_packages, setup
+
+setup(
+    name = "filebin-cli",
+    version = "0.1",
+    packages = find_packages(),
+    install_requires = [
+        "click",
+        "requests"
+    ],
+    entry_points = {
+        "console_scripts": [
+            'filebin = filebin.main:main',  # 'command = module:function'
+        ],
+    },
+    author = "https://github.com/mshirazkamran",
+    description = "A cli tool to temporarily share file wihout any hassle. Uses filebin.net public api"
+)
